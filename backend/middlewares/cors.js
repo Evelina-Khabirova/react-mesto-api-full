4b3a844e6eb510074c16f79ad8221d0614b6492b
Khabirova Evelina <evelina.khabirova@yandex.ru>
2022-10-28 22:56:09 +0300

    убрать s из ссылки в корсе

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,8 @@
-const allowedCors = ['https://praktikum.tk', 'http://praktikum.th', 'http://localhost:7777', 'http://api.mesto.evelina.nomoredomains.icu', 'https://mesto.evelina-khabirova.nomoredomains.icu', 'http://127.0.0.1:7777', 'http://localhost:5555', 'http://127.0.0.1:5555', 'http://localhost:5555/signup', 'http://localhost:5555/signin'];
-const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
+const allowedCors = ['https://praktikum.tk', 'http://praktikum.th', 'http://localhost:7777', 'http://api.mesto.evelina.nomoredomains.icu', 'http://mesto.evelina-khabirova.nomoredomains.icu', 'http://127.0.0.1:7777', 'http://localhost:5555', 'http://127.0.0.1:5555', 'http://localhost:5555/signup', 'http://localhost:5555/signin'];
 module.exports.cors = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
