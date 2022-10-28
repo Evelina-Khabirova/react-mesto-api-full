3c74904fb7952630381813703705a2d14be38b94
Khabirova Evelina <evelina.khabirova@yandex.ru>
2022-10-28 21:51:16 +0300

    Поправить корс

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,18 +4,14 @@ module.exports.cors = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   if (method === 'OPTIONS') {
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Credentials', true);
     return res.end();
-  }
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
   return next();
 };
