f1ca3b9f6afcb87ade4716e6cec9497bb7bc6d07
Khabirova Evelina <evelina.khabirova@yandex.ru>
2022-10-29 22:19:08 +0300

    добавить s

--- backend/middlewares/handleCors.js
+++ backend/middlewares/handleCors.js
@@ -5,13 +5,13 @@ module.exports.handleCors = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.headers('Access-Control-Allow-Origin', origin);
+    res.headers('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.headers('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.headers('Access-Control-Allow-Headers', requestHeaders);
+    res.headers('Access-Control-Allow-Credentials', true);
     return res.end();
   }
   return next();
