1c49b445ca1359fbacee53a0a785d5183bc90748
Khabirova Evelina <evelina.khabirova@yandex.ru>
2022-10-29 18:49:08 +0300

    cors

--- backend/middlewares/handleCors.js
+++ backend/middlewares/handleCors.js
@@ -1,4 +1,4 @@
-const allowedCors = ['https://praktikum.tk', 'http://praktikum.th', 'http://localhost:7777', 'http://api.mesto.evelina.nomoredomains.icu', 'http://mesto.evelina-khabirova.nomoredomains.icu', 'http://127.0.0.1:7777'];
+const allowedCors = ['https://praktikum.tk', 'http://praktikum.th', 'http://localhost:7777', 'http://api.mesto.evelina.nomoredomains.icu', 'http://mesto.evelina-khabirova.nomoredomains.icu', 'http://127.0.0.1:7777', 'http://localhost:5555', 'http://127.0.0.1:5555'];
 module.exports.handleCors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
@@ -6,10 +6,12 @@ module.exports.handleCors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Credentials', true);
     return res.end();
   }
   return next();
